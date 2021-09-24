aa94c64697512ae086a530ed8cebc86f06823148
Jinto Jose <jintoppy@gmail.com>
2021-09-24 11:53:02 +0530

    character pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderStyleDirective } from './header-style.directive';
 import { FirstLetterPipe } from './first-letter.pipe';
+import { CharacterCountPipe } from './character-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderStyleDirective,
-    FirstLetterPipe
+    FirstLetterPipe,
+    CharacterCountPipe
   ],
   imports: [
     BrowserModule
